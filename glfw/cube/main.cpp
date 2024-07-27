edd8305e6f2bb29b49e216fb62037af613d5bd7a
Jens <jens.marhoefer001@gmail.com>
2024-07-27 13:52:21 +0200

    -

--- glfw/cube/main.cpp
+++ glfw/cube/main.cpp
@@ -4,26 +4,97 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 
 using namespace std;
 
-#define numVAOs 1
 
+#define numVAOs 1
+#define numVBOs 2
+
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocz;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+//allocate variables used in display() function, so that they won't need to be allocated during rendering
+
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+
+void setupVertices(void){
+    float vertexPositions[108] = {
+        -1.0f, -1.0f,  1.0f, // Triangle 1
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f, // Triangle 2
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        // Back face
+        -1.0f, -1.0f, -1.0f, // Triangle 1
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f, // Triangle 2
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+
+        // Left face
+        -1.0f, -1.0f, -1.0f, // Triangle 1
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f, // Triangle 2
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+
+        // Right face
+         1.0f, -1.0f, -1.0f, // Triangle 1
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f, // Triangle 2
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+
+        // Top face
+        -1.0f,  1.0f, -1.0f, // Triangle 1
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f, // Triangle 2
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        // Bottom face
+        -1.0f, -1.0f, -1.0f, // Triangle 1
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f, // Triangle 2
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f
+    };
+
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(vao[0]);
+    glGenBuffers(numVBOs, vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+}
 
 
 void init(GLFWwindow* window) {
     renderingProgram = createShaderProgram("shaders/default.vert", "shaders/default.frag");
-    //When sets of data are prepared for sending down the pipeline, they are organized into buffers.
-    //Those buffers are in turn organized into Vertex Array Objects.
-    glGenVertexArrays(numVAOs, vao);
-    glBindVertexArray(vao[0]);
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+    cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+    setupVertices();
 }
 
-float x = 0.0f;
-float y = 0.0f;
-float inc = 0.01f;
 
 void display(GLFWwindow* window, double currentTime) {
     glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -32,18 +103,16 @@ void display(GLFWwindow* window, double currentTime) {
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     //glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(renderingProgram);
-    
-    x += inc;
-    y += inc;
-    if (x > 0.5f) inc = -0.00005f;
-    if (x < 0.0f) inc =  0.00005f;
-   
-    
-    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
-    GLuint offsetLoc2 = glGetUniformLocation(renderingProgram, "offset2");
-    glProgramUniform1f(renderingProgram, offsetLoc, x);
-    glProgramUniform1f(renderingProgram, offsetLoc2, y);
-    
+
+
+    //get the uniform variables for the MV and projection matrices
+    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix")
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix")
+
+    //build perspective matrix      
+    glfwGetFramebufferSize(window, &width, &height);
+
+
     //glLineWidth(30.0f);
     //glPointSize(30.0f);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
--- glfw/cube/shaders/default.vert
+++ glfw/cube/shaders/default.vert
@@ -1,9 +1,5 @@
 #version 430 core
 
-uniform float offset;
-uniform float offset2;
-
-float vertex_pos = 0.01;
 
 void main(void) {
     if (gl_VertexID == 0) gl_Position = vec4(vertex_pos + offset, -vertex_pos - offset, 0.0, 1.0);
